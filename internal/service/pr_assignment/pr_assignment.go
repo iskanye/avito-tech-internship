fd96530e4c2402814b0ec3379230b170233234c2
iskanye <iskanyeban@gmail.com>
2025-11-20 15:24:48 +0300

    feat: service struct

---
+++ internal/service/pr_assignment/pr_assignment.go
@@ -0,0 +1,56 @@
+package prassignment
+
+import (
+	"context"
+
+	"github.com/iskanye/avito-tech-internship/internal/models"
+)
+
+type PRAssignment struct {
+	// Объекты для взаимодействия с пользователями
+	userCreator  UserCreator
+	userModifier UserModifier
+	userProvider UserProvider
+
+	// Объекты для взаимодействия с командами
+	teamCreator  TeamCreator
+	teamProvider TeamProvider
+}
+
+// Интерфейсы для работы сервиса
+
+type UserCreator interface {
+	AddUser(
+		ctx context.Context,
+		user models.User,
+	) error
+}
+
+type UserProvider interface {
+	GetUser(
+		ctx context.Context,
+		userID string,
+	) (models.User, error)
+}
+
+type UserModifier interface {
+	SetActive(
+		ctx context.Context,
+		userID string,
+		isActive bool,
+	) error
+}
+
+type TeamCreator interface {
+	AddTeam(
+		ctx context.Context,
+		teamName string,
+	) (int64, error)
+}
+
+type TeamProvider interface {
+	GetTeam(
+		ctx context.Context,
+		teamName string,
+	) (models.Team, error)
+}
