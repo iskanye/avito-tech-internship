e40c77ada06895f6e360cfe5e6be3582d54d4335
iskanye <iskanyeban@gmail.com>
2025-11-20 10:45:55 +0300

    feat: init migrations

---
+++ cmd/migrator/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/iskanye/avito-tech-internship/internal/config"
+)
+
+func main() {
+	var configPath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&configPath, "config", "", "path to config")
+	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
+	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of migrations table")
+	flag.Parse()
+
+	if configPath == "" {
+		configPath = os.Getenv("CONFIG_PATH")
+	}
+
+	cfg := config.MustLoadPath(configPath)
+	cfg.LoadEnv()
+
+	uri := fmt.Sprintf("%s:%s@%s:%d/%s",
+		cfg.Postgres.User,
+		cfg.Postgres.Password,
+		cfg.Postgres.Host,
+		cfg.Postgres.Port,
+		cfg.Postgres.DBName,
+	)
+
+	if migrationsPath == "" {
+		panic("migrations-path is required")
+	}
+
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("postgres://%s?x-migrations-table=%s&sslmode=disable", uri, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	mustMigrate(m.Up())
+}
+
+func mustMigrate(err error) {
+	if err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied")
+}
---
+++ migrations/1_init.up.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS users_id
+(
+    id SERIAL PRIMARY KEY,
+    user_id TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users_id (id),
+    team_id INTEGER REFERENCES teams (id),
+    is_active BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS teams
+(
+    id SERIAL PRIMARY KEY,
+    team_name NAME
+);
