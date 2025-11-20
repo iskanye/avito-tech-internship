f18b5499a1a485711a2f93f74e63bac7da58fe4f
iskanye <iskanyeban@gmail.com>
2025-11-21 02:16:36 +0300

    feat (server): TeamAdd

--- internal/server/team.go
+++ internal/server/team.go
@@ -2,16 +2,49 @@ package server
 
 import (
 	"context"
+	"errors"
 
+	"github.com/iskanye/avito-tech-internship/internal/models"
+	prassignment "github.com/iskanye/avito-tech-internship/internal/service/pr_assignment"
 	"github.com/iskanye/avito-tech-internship/pkg/api"
 )
 
 // (POST /team/add)
-func (serverAPI) PostTeamAdd(
+func (s *serverAPI) PostTeamAdd(
 	c context.Context,
 	req api.PostTeamAddRequestObject,
 ) (api.PostTeamAddResponseObject, error) {
-	return nil, nil
+	teamReq := models.Team{
+		TeamName: req.Body.TeamName,
+		Members:  make([]models.User, len(req.Body.Members)),
+	}
+	for i, member := range req.Body.Members {
+		teamMember := models.User{
+			UserID:   member.UserId,
+			Username: member.Username,
+			IsActive: member.IsActive,
+		}
+		teamReq.Members[i] = teamMember
+	}
+
+	team, err := s.assign.AddTeam(c, teamReq)
+	if errors.Is(err, prassignment.ErrTeamExists) {
+		return api.PostTeamAdd400JSONResponse{
+			Error: struct {
+				Code    api.ErrorResponseErrorCode "json:\"code\""
+				Message string                     "json:\"message\""
+			}{
+				Code:    api.TEAMEXISTS,
+				Message: err.Error(),
+			},
+		}, err
+	}
+
+	teamResp := convertTeamToApi(&team)
+	response := api.PostTeamAdd201JSONResponse{
+		Team: teamResp,
+	}
+	return response, nil
 }
 
 // (GET /team/get)
@@ -20,4 +53,21 @@ func (serverAPI) GetTeamGet(
 	req api.GetTeamGetRequestObject,
 ) (api.GetTeamGetResponseObject, error) {
 	return nil, nil
+}
+
+func convertTeamToApi(team *models.Team) *api.Team {
+	teamRes := api.Team{
+		TeamName: team.TeamName,
+		Members:  make([]api.TeamMember, len(team.Members)),
+	}
+	for i, member := range team.Members {
+		teamMember := api.TeamMember{
+			UserId:   member.UserID,
+			Username: member.Username,
+			IsActive: member.IsActive,
+		}
+		teamRes.Members[i] = teamMember
+	}
+
+	return &teamRes
 }
