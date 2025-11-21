87476994bc6919d23811dc808a808e7e9ba52257
iskanye <iskanyeban@gmail.com>
2025-11-21 13:22:08 +0300

    fix: team exists error

--- internal/service/prassignment/errors.go
+++ internal/service/prassignment/errors.go
@@ -4,7 +4,7 @@ import "errors"
 
 var (
 	ErrNotFound   = errors.New("resource not found")
-	ErrTeamExists = errors.New("team already exists")
+	ErrTeamExists = errors.New("team_name already exists")
 	ErrPRExists   = errors.New("PR is already exists")
 	ErrPRMerged   = errors.New("cannot reassign on merged PR")
 )
